8b718c0c4f6afe1f67c033a3f1b9fb668352e4b2
Rifat172 <49565552+Rifat172@users.noreply.github.com>
2019-04-14 23:24:34 +0500

    создание первого шаблона mvvm

---
+++ PlayerView/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerView.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private string _synchronizedText;
+        public string SynchronizedText
+        {
+            get => _synchronizedText;
+            set
+            {
+                _synchronizedText = value;
+                OnPropertyChanged(nameof(SynchronizedText));
+            }
+        }
+    }
+}
